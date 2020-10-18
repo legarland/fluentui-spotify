2150337c0ceedb63cbf59a43abd56440cdc552c9
Logan Garland <legarland@gmail.com>
2020-10-18 01:43:49 -0700

    unused variables

--- src/components/TrackCard.js
+++ src/components/TrackCard.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { Image, Stack, Text } from '@fluentui/react'
-import { Card } from '@uifabric/react-cards'
-import { useSelector } from 'react-redux'
+// import { Image, Stack, Text } from '@fluentui/react'
+// import { Card } from '@uifabric/react-cards'
+// import { useSelector } from 'react-redux'
 import useSpotify from '../hooks/useSpotify'
-import { myTheme } from '../lib/theme'
-import PlayButton from './PlayButton'
+// import { myTheme } from '../lib/theme'
+// import PlayButton from './PlayButton'
 import GeneralCard from './GeneralCard'
 
 const TrackCard = ({ id }) => {
   const [data, setData] = useState()
-  const currentTrack = useSelector((state) => state.playback.track)
-  const isPlaying = useSelector((state) => state.playback.isPlaying)
+  // const currentTrack = useSelector((state) => state.playback.track)
+  // const isPlaying = useSelector((state) => state.playback.isPlaying)
   const spotify = useSpotify()
   useEffect(() => {
     if (spotify) spotify.getTrack(id).then((d) => setData(d))
